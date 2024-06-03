12c29eeaabc250b668ec3aa71d85eb704a4262f6
Denis Protopopov <protdenden@gmail.com>
2024-06-03 17:14:15 +0400

    final part

--- script.js
+++ script.js
@@ -6,6 +6,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('intro-text').innerText = introTexts[0];
 
+    const outroText = `IDEO / Fluxus / George Maciunas / Robert Rauschenberg / 
+    Håkan Lidbo / Takako Saito / John Cage / Marcel Duchamp / James Nares / Ben Patterson / Igor Krutogolov's Toy Orchestra /  Nam June Paik / Katya Shirshkova / David Cranmer / Michael Rucci / Circuit Bending Fool / Psychiceyeclix / 
+    Moon Armada / ELECTRONICOS FANTASTICOS! / Roman Gerold / Dennis Oppenheim / Laurie Anderson / Yoko Ono / 
+    Allan Kaprow / Tristan Tzara / Kurt Schwitters / Trevor Wishart / Pauline Oliveros / Hanne Lippard / 
+    Hugo Ball / Earle Brown / Virgil Thomson, Henry Cowell, and Lou Harrison / Brian Eno, Peter Schmidt / 
+    Josef Haydn / Mieko Shiomi / Alison Knowles / Petr Kotik / Vytautas Landsbergis / Hildegard Westerkamp  / 
+    Pugacheva / Ken Friedman / Protopopov`;
+
+    const lastPart = {
+        first: `Часть 11 — Final — мы сделаем с вами партитуру? `,
+        second: `<div class="circle">Hildegard Westerkamp <br>
+        Try to move <br>
+        Without making any sound. <br>
+        Is it possible?<br>
+        Do you hear any<br>
+        Intermittent or discrete sounds<br>
+        Rustles<br>
+        Bangs<br>
+        Swishes<br>
+        Thuds<br>
+        What are the sources of the different sounds?<br>
+        What else do you hear?<br>
+        Lead your ears away from these sounds and listen<br>
+        beyond-----into the distance.<br>
+        What is the quietest sound?<br>
+        What else do you hear?<br>
+        What else?<br>
+        What else?<br>
+        What else?<br>
+        What else? </div>
+        `,
+        third: `<div class="circle">по русски</div>`
+    };
+
     document.addEventListener('click', handleDocumentClick);
     document.addEventListener('keydown', handleKeyDown);
 
@@ -49,29 +83,70 @@ document.addEventListener('DOMContentLoaded', () => {
             изысканный труп / <span class='circle'>партитуры</span> / circuit bending / колода карт / слова / <span class='circle'>сломанный телефон</span> / 
             правила / <span class='circle'>музыкальный автомат</span> / звуковые игры / квесты / <span class='circle'>выбор</span> / <span class='circle'>интерактивность</span> / 
             шахматы / игральные кости / лего / <span class='circle'>тв-игра</span> / звуковая поэзия / куклы / звуковые прогулки`,
-            third: `Pauline Oliveros: <br>
+            third: `<div class="circle">Pauline Oliveros: <br>
             FOR ANNEA LOCKWOOD AND ALISON KNOWLES<br>
             Keep the next sound you hear in mind<br>
             for at least the next halfhour.<br>
-            February 20, 1975
+            February 20, 1975</div>
             `,
-            fourth: `по русски`
+            fourth: `<div class="circle">по русски</div>`
         },
-        // Add more parts as needed
+        {
+            first: `Часть 7 Игры слов ???`,
+            second: `непредсказуемость / музыкальные игры / хакинг / камни / шары / звуковые карты / 
+            спортивные игры / синтезаторы / случайность / <span class='circle'>инструкции</span> / игрушки / монетка / память / 
+            изысканный труп / <span class='circle'>партитуры</span> / circuit bending / колода карт / <span class='circle'>слова</span> / сломанный телефон / 
+            правила / музыкальный автомат / звуковые игры / квесты / выбор / интерактивность / 
+            шахматы / игральные кости / лего / тв-игра / <span class='circle'>звуковая поэзия</span> / куклы / звуковые прогулки`
+        },
+        {
+            first: `Часть 8 Музыкальные игры или что-то на композиторском`,
+            second: `<span class='circle'>непредсказуемость</span> / <span class='circle'>музыкальные игры</span> / хакинг / камни / шары / звуковые карты / 
+            спортивные игры / синтезаторы / <span class='circle'>случайность</span> / инструкции / игрушки / <span class='circle'>монетка</span> / память / 
+            <span class='circle'>изысканный труп</span> / партитуры / circuit bending / <span class='circle'>колода карт</span> / слова / сломанный телефон / 
+            правила / музыкальный автомат / звуковые игры / квесты / <span class='circle'>выбор</span> / интерактивность / 
+            шахматы / <span class='circle'>игральные кости</span> / лего / тв-игра / звуковая поэзия / куклы / звуковые прогулки`,
+            third: `<div class="circle">In total darkness, or in a very large room, very quietly // Be less critical more often</div>`,
+            fourth: `<div class="circle">по русски</div>`
+        },
+        {
+            first: `Часть 9 Видеоигры без видео или играя в звук на компьютере`,
+            second: `непредсказуемость / музыкальные игры / хакинг / камни / шары / звуковые карты / 
+            спортивные игры / <span class='circle'>синтезаторы</span> / случайность / <span class='circle'>инструкции</span> / игрушки / монетка / <span class='circle'>память</span> / 
+            изысканный труп / партитуры / circuit bending / колода карт / слова / сломанный телефон / 
+            <span class='circle'>правила</span> / музыкальный автомат / <span class='circle'>звуковые игры</span> / квесты / <span class='circle'>выбор</span> / <span class='circle'>интерактивность</span> / 
+            шахматы / игральные кости / лего / тв-игра / звуковая поэзия / куклы / звуковые прогулки`
+        },
+        {
+            first: `Часть 10 — Все по правилам или для начала правила`,
+            second: `непредсказуемость / музыкальные игры / хакинг / камни / шары / <span class='circle'>звуковые карты</span> / 
+            спортивные игры / синтезаторы / случайность / <span class='circle'>инструкции</span> / игрушки / монетка / память / 
+            изысканный труп / <span class='circle'>партитуры</span> / circuit bending / колода карт / слова / сломанный телефон / 
+            <span class='circle'>правила</span> / музыкальный автомат / звуковые игры / <span class='circle'>квесты</span> / выбор / интерактивность / 
+            шахматы / игральные кости / лего / тв-игра / звуковая поэзия / куклы / <span class='circle'>звуковые прогулки</span>`,
+            third: `<div class="circle">Ken Friedman — Mandatory Happening — A card printed: You will decide to read this score or not to read it. When you have made your decision, the happening is over. (1966)</div>`,
+            fourth: `<div class="circle">по русски</div>`
+        }
+
     ];
 
     let availableParts = [...parts];
-    let usedParts = []; // Track used parts
     let currentPart = null;
     let currentBlock = 0;
     let introStep = 0;  // Track the introduction step
     let historyStack = [];  // Stack to keep track of history
+    let lastPartShown = false; // Track if the last part has been shown
+    let outroShown = false; // Track if the outro has been shown
 
     function handleDocumentClick(event) {
         if (introStep < introTexts.length) {
             showIntro();
-        } else {
+        } else if (availableParts.length > 0 || (currentPart === lastPart && currentBlock < Object.keys(lastPart).length - 1)) {
             showNextBlock();
+        } else if (!lastPartShown) {
+            showLastPart();
+        } else if (!outroShown) {
+            showOutro();
         }
     }
 
@@ -93,24 +168,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function showNextBlock() {
-        historyStack.push({ currentPart, currentBlock, availableParts: [...availableParts] });
+        if (currentPart) {
+            historyStack.push({ currentPart, currentBlock, availableParts: [...availableParts], lastPartShown, outroShown });
 
-        currentBlock++;
-        if (!currentPart || currentBlock >= Object.keys(currentPart).length) {
-            if (currentPart) {
-                // Only move to usedParts after fully displaying the part
-                usedParts.push(currentPart);
-                availableParts = availableParts.filter(part => part !== currentPart);
+            currentBlock++;
+            if (currentBlock >= Object.keys(currentPart).length) {
+                if (currentPart === lastPart) {
+                    showOutro();
+                } else {
+                    showRandomPart();
+                }
+            } else {
+                displayBlock();
             }
-            showRandomPart();
-        } else {
-            displayBlock();
         }
     }
 
     function showRandomPart() {
         if (availableParts.length === 0) {
-            document.getElementById('text-block').innerHTML = "No more parts available.";
+            showLastPart();
             return;
         }
 
@@ -118,7 +194,23 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPart = availableParts[randomIndex];
         currentBlock = 0;
 
+        availableParts.splice(randomIndex, 1); // Remove the part from availableParts
+
         displayBlock();
+    }
+
+    function showLastPart() {
+        currentPart = lastPart;
+        currentBlock = 0;
+        lastPartShown = true;
+        displayBlock();
+    }
+
+    function showOutro() {
+        outroShown = true;
+        document.getElementById('text-block-container').style.display = 'none';
+        document.getElementById('text-block').style.display = 'block';
+        document.getElementById('text-block').innerText = outroText;
     }
 
     function displayBlock() {
@@ -128,6 +220,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (blockKeys[currentBlock] === "third" && blockKeys[currentBlock + 1] === "fourth") {
                 // If third block has both left and right parts
+                document.getElementById('text-block').style.display = 'none';
+                document.getElementById('text-block-container').style.display = 'flex';
+                document.getElementById('left-block').innerHTML = blockText;
+                document.getElementById('right-block').innerHTML = currentPart[blockKeys[currentBlock + 1]];
+                currentBlock++; // Skip the next block since it's already handled
+            } else if (currentPart === lastPart && blockKeys[currentBlock] === "second" && blockKeys[currentBlock + 1] === "third") {
+                // Handle special case for the last part
                 document.getElementById('text-block').style.display = 'none';
                 document.getElementById('text-block-container').style.display = 'flex';
                 document.getElementById('left-block').innerHTML = blockText;
@@ -147,6 +246,8 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPart = lastState.currentPart;
             currentBlock = lastState.currentBlock;
             availableParts = lastState.availableParts;
+            lastPartShown = lastState.lastPartShown;
+            outroShown = lastState.outroShown;
             displayBlock();
         }
     }
